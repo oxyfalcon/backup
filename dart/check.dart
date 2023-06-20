c4004f91ff06206bffe5c74367bdebd4e168a0e5
oxyfalcon <akashsinghcool19990@gmail.com>
2023-06-20 22:18:29 +0530

    Async_programming_2

--- dart/check.dart
+++ dart/check.dart
@@ -1,22 +1,42 @@
+dynamic ans2;
+
+Stream<int> itr2(int n) async* {
+  if (n <= 0) {
+    yield 1;
+  }
+  yield* itr2(n + 1);
+}
+
+dynamic ans;
 Future<int> itr(int n) async {
   if (n <= 0) {
     return n;
   }
-  print(n);
-  return itr(n + 1);
+  // print(n);
+  try {
+    ans = await itr(n + 1);
+    return ans;
+  } on StackOverflowError catch (e) {
+    print("$e, ERROR: 1");
+    // print("$s, in Error 1");
+    rethrow;
+  }
+  //
 }
 
 void main() async {
+  // itr(10);
   try {
     var result = await itr(10);
     print(result);
-  } catch (e) {
-    print("$e, error");
-  }
-  // result.then((value) => print(value)).catchError((error) {
-  //   print('First catch block');
-  //   print('$error ERROR 1');
-  // });
+    // await result.then((value) => print(value)).catchError((error) {
+    //   print('First catch block');
+    //   print('$error ERROR 2');
+    // });
 
-  print("i m here");
+    //   // print(result);
+  } catch (e) {
+    print("$e, ERROR: 2");
+  }
+  // print("i m here");
 }
