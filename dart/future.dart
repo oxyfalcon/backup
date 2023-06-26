b7577e7efef46fecf25f4089c580d614fdb83cc0
oxyfalcon <akashsinghcool19990@gmail.com>
2023-06-26 18:52:01 +0530

    Word_Generator_app

--- dart/future.dart
+++ dart/future.dart
@@ -3,17 +3,18 @@ Future<int> divider() {
 }
 
 Future<List<int>> getList(List<int> list) async {
-  var number = divider().then((number) => List<int>.from(
-      list.where((element) => element % number == 0 ? true : false)));
+  var number = divider()
+      .then((number) => List<int>.from(
+          list.where((element) => element % number == 0 ? true : false)))
+      .then((value) => print("From Future Class: $value"));
   print("From Future Class: $number");
   // var ans = List<int>.from(
   //     list.where((element) => element % number == 0 ? true : false));
   return number;
 }
 
-int global = 10;
 Future<int> temp() {
-  return Future.delayed(Duration(seconds: 10), () => global);
+  return Future.delayed(Duration(seconds: 10), () => 10);
 }
 
 Stream<int> stream() async* {
@@ -28,7 +29,8 @@ void main() async {
   temp().then((value) => print("temp val returned: $value"));
   print("temp val instance: ${temp()}");
 
-  getList(l).then(
+  var x = getList(l);
+  x.then(
     (value) async {
       var s = stream();
       await for (int val in s) {
@@ -36,6 +38,8 @@ void main() async {
       }
     },
   );
+
+  print(x);
 
   // var s = stream();
   // await for (int val in s) {
