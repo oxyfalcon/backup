f965346ac6b81a5002bc977f1dffad1d757a994a
oxyfalcon <103992670+oxyfalcon@users.noreply.github.com>
2023-12-18 15:14:12 +0530

    removed layout builder

--- flutter_projects/youtube_clone/lib/Homepage/revenue_widget/pie_chart/chart_and_lebel.dart
+++ flutter_projects/youtube_clone/lib/Homepage/revenue_widget/pie_chart/chart_and_lebel.dart
@@ -9,92 +9,89 @@ class ChartAndLebel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, cons) {
-      return Row(
-        children: [
-          Expanded(
-              flex: 3,
-              child: Container(
-                constraints: BoxConstraints.loose(const Size.fromHeight(150)),
-                child: const FractionallySizedBox(
-                  widthFactor: 0.8,
-                  heightFactor: 0.8,
-                  child: DonutChart(),
-                ),
-              )),
-          Expanded(
-            flex: 5,
-            child: Column(
-              children: [
-                for (var i in valueToChart.entries)
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "\u2022",
-                            style: TextStyle(
-                                color: i.value,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            "  ${i.key.entries.first.key}%",
-                            style: const TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Flexible(
-                            child: Row(
-                              children: [
-                                Flexible(
-                                  flex: 3,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 10.0),
-                                    child: Text(
-                                      i.key.entries.first.value,
-                                      style: const TextStyle(
-                                          fontSize: 14,
-                                          fontFamily: "Arial1",
-                                          fontWeight: FontWeight.w600,
-                                          color: ColorMap.cardSecondaryText),
-                                    ),
+    return Row(
+      children: [
+        Expanded(
+            flex: 3,
+            child: Container(
+              constraints: BoxConstraints.loose(const Size.fromHeight(150)),
+              child: const FractionallySizedBox(
+                widthFactor: 0.8,
+                heightFactor: 0.8,
+                child: DonutChart(),
+              ),
+            )),
+        Expanded(
+          flex: 5,
+          child: Column(
+            children: [
+              for (var i in valueToChart.entries)
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "\u2022",
+                          style: TextStyle(
+                              color: i.value,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "  ${i.key.entries.first.key}%",
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Flexible(
+                          child: Row(
+                            children: [
+                              Flexible(
+                                flex: 3,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 10.0),
+                                  child: Text(
+                                    i.key.entries.first.value,
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        fontFamily: "Arial1",
+                                        fontWeight: FontWeight.w600,
+                                        color: ColorMap.cardSecondaryText),
                                   ),
                                 ),
-                                if (i.key.entries.first.key == 15)
-                                  Flexible(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 10.0),
-                                      child: IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(
-                                              Icons.arrow_forward_ios,
-                                              size: 14)),
-                                    ),
+                              ),
+                              if (i.key.entries.first.key == 15)
+                                Flexible(
+                                  flex: 1,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 10.0),
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                            Icons.arrow_forward_ios,
+                                            size: 14)),
                                   ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      if (i.key.entries.first.key != 15)
-                        const Padding(
-                          padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                          child: CustomDottedLine(
-                              lineThickness: 0.5,
-                              dashLength: 2,
-                              dashGapLength: 2,
-                              color: ColorMap.listTileShadow),
+                                ),
+                            ],
+                          ),
                         )
-                    ],
-                  ),
-              ],
-            ),
-          )
-        ],
-      );
-    });
+                      ],
+                    ),
+                    if (i.key.entries.first.key != 15)
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                        child: CustomDottedLine(
+                            lineThickness: 0.5,
+                            dashLength: 2,
+                            dashGapLength: 2,
+                            color: ColorMap.listTileShadow),
+                      )
+                  ],
+                ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
 
